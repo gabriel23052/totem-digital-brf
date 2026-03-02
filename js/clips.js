b56106d4106517914e9654c8be84da1a5d613367
Gabriel <gabrielggp2002@gmail.com>
2026-03-02 17:10:17 -0300

    Commit inicial

---
+++ js/clips.js
@@ -0,0 +1,14 @@
+const clips = [
+  {start: 10, end: 20},
+  {start: 0, end: 0},
+  {start: 0, end: 0},
+  {start: 0, end: 0},
+  {start: 0, end: 0},
+  {start: 0, end: 0},
+  {start: 0, end: 0},
+  {start: 0, end: 0},
+  {start: 0, end: 0},
+  {start: 0, end: 0},
+  {start: 0, end: 0},
+  {start: 0, end: 0},
+]
